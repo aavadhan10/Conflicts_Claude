4279a9f625d35f6ddeca2a2fb2d5137f4186ceb3
aavadhan10 <166965425+aavadhan10@users.noreply.github.com>
2024-09-16 11:41:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ if st.button("Check for Conflict"):
             
                 if additional_info is not None and not additional_info.empty:
                     st.write("#### Potential Opponents, Direct Opponents, Business Owners, and Acquisition Parties:")
-                    st.dataframe(additional_info, width=1000, height=400)
+                    st.table(additional_info)  # Automatically adjusts table size to fit content
             else:
                 st.write("No additional parties or conflicts identified.")
     else:
